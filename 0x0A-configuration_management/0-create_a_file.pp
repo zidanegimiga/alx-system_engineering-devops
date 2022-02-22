adb122baa45b4701de63474cb7defdfb9e0be38c
Zidane Gimiga <33351058+zidanegimiga@users.noreply.github.com>
2022-02-22 06:05:39 +0300

    Creating  a file using Puppet

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# Using Puppet, create a file in '/tmp'.
+file { '/tmp/holberton':
+  ensure  => 'file',
+  path    => '/tmp/holberton',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
